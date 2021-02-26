1fc8d9aff0e6db81638d02a277a029ff479912d2
indunil-19 <induniludayangana.18@cse.mrt.ac.lk>
2021-02-26 13:43:39 +0530

    test order change

--- test/integration tests (controller+services+models)/common/Root.test.js
+++ test/integration tests (controller+services+models)/common/Root.test.js
@@ -47,19 +47,30 @@ describe('root Controller', () => {
         });
 
     });
-    describe('Load Credits Page Functionality : RootController creditpage Method;', ()=>{
-        it('should render _credits_aboutus if any error',()=>{
+
+    describe('User View Login Page Funtionality : RootController login page Method;', ()=>{
+        it('should render login page with error,and email if any error ',()=>{
             const req={
-                query:{error:'test error'},
+                query:{error:'test error',
+                       email:'test@gmail.com',
+                       adminRegSuccess:"admin register successful",
+                       farmerRegSuccess:"farmer register successful",
+                       buyerRegSuccess:"buyer register successful",
+                       del_acc_success:"delete account successful"},
                 session:{user:{uid:1}}
             }
-            rootController.creditPage(req,res)
-            expect(res.render).toHaveBeenCalledWith('_credits_aboutus',{
+            rootController.loginPage(req,res);
+            expect(res.render).toHaveBeenCalledWith('login',{
                 error:'test error',
+                email:'test@gmail.com',
                 user:{uid:1},
+                adminRegSuccess:"admin register successful",
+                farmerRegSuccess:"farmer register successful",
+                buyerRegSuccess:"buyer register successful",
+                del_acc_success:"delete account successful"
             })
         })
-    })
+    });
     describe('User Login Funtionality : RootController login Method;', ()=>{
         let req;
         beforeEach(()=>{
@@ -104,49 +115,7 @@ describe('root Controller', () => {
 
 
     })
-    describe('User Logout Funtionality : RootController logout Method;',()=>{
-        let req;
-        beforeEach(()=>{
-            req={
-                session:{
-                    user:{}
-                }
-            }
-        });
-        it('should redirect to index page if no error',async()=>{
-            await rootController.logout(req,res)
-            expect(res.redirect).toHaveBeenCalledWith(expect.stringMatching(/[/]/))
-        })
-        it('should redirect to index page if any error',async()=>{
-            req.session=null
-            await rootController.logout(req,res)
-            expect(res.redirect).toHaveBeenCalledWith(expect.stringMatching(/[/]/))
-        })
-        
-    })
-    describe('User View Login Page Funtionality : RootController login page Method;', ()=>{
-        it('should render login page with error,and email if any error ',()=>{
-            const req={
-                query:{error:'test error',
-                       email:'test@gmail.com',
-                       adminRegSuccess:"admin register successful",
-                       farmerRegSuccess:"farmer register successful",
-                       buyerRegSuccess:"buyer register successful",
-                       del_acc_success:"delete account successful"},
-                session:{user:{uid:1}}
-            }
-            rootController.loginPage(req,res);
-            expect(res.render).toHaveBeenCalledWith('login',{
-                error:'test error',
-                email:'test@gmail.com',
-                user:{uid:1},
-                adminRegSuccess:"admin register successful",
-                farmerRegSuccess:"farmer register successful",
-                buyerRegSuccess:"buyer register successful",
-                del_acc_success:"delete account successful"
-            })
-        })
-    });
+  
     describe('User View Edit Profile Page Funtionality : RootController editprofilepage Method;', ()=>{
         it('should render userEditProfile if any error',()=>{
             const req={
@@ -247,6 +216,39 @@ describe('root Controller', () => {
         await sql`INSERT INTO UserInfo (uid,email, type, password, first_name,last_name,gender) VALUES ('00000000-0000-4000-8000-000000000007','test7@gmail.com','buyer','$2b$10$dJOQi73zTgi.tWOMK57yVeQOOtqCuLUcfREUKTiSMwwLUtGqK/A8K','testFirstName7','testlastName7','Male');
         `
     })
+    })
+    describe('User Logout Funtionality : RootController logout Method;',()=>{
+        let req;
+        beforeEach(()=>{
+            req={
+                session:{
+                    user:{}
+                }
+            }
+        });
+        it('should redirect to index page if no error',async()=>{
+            await rootController.logout(req,res)
+            expect(res.redirect).toHaveBeenCalledWith(expect.stringMatching(/[/]/))
+        })
+        it('should redirect to index page if any error',async()=>{
+            req.session=null
+            await rootController.logout(req,res)
+            expect(res.redirect).toHaveBeenCalledWith(expect.stringMatching(/[/]/))
+        })
+        
+    })
+    describe('Load Credits Page Functionality : RootController creditpage Method;', ()=>{
+        it('should render _credits_aboutus if any error',()=>{
+            const req={
+                query:{error:'test error'},
+                session:{user:{uid:1}}
+            }
+            rootController.creditPage(req,res)
+            expect(res.render).toHaveBeenCalledWith('_credits_aboutus',{
+                error:'test error',
+                user:{uid:1},
+            })
+        })
     })
 
 });
